3bca882f8dc8bd2f0b871d0b5d1f868e00514563
Chase Andeen <chase.andeen@proton.me>
2026-07-27 19:35:24 +0000

    Added file read

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath: str) -> str:
+    with open(filepath) as file:
+        data = file.read()
+    return data
+
+def main():
+    filepath = "books/frankenstein.txt"
+    book_text = get_book_text(filepath)
+    print(book_text)
+
+main()
