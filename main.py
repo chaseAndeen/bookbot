c0e1a7a79b706ae287e066518e060d3352d87697
Chase Andeen <chase.andeen@proton.me>
2026-07-31 14:07:17 +0000

    Add character count functionality and update main to display it

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import word_count
+from stats import char_count
+
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as file:
@@ -9,5 +11,6 @@ def main():
     filepath = "books/frankenstein.txt"
     book_text = get_book_text(filepath)
     print(f"Found {word_count(book_text)} total words")
+    print(f"{char_count(book_text)}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def word_count(text: str) -> int:
     words = text.split()
     return len(words)
+
+def char_count(text: str) -> dict[str, int]:
+    char_totals = {}
+    chars = list(text.lower())
+
+    for c in chars:
+        if c not in char_totals:
+            char_totals[c] = 1
+        else:
+            char_totals[c] += 1
+
+    return char_totals
