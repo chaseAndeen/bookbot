65adf0177f0213b6efaa17c67c03e6ad8dce52ef
Chase Andeen <chase.andeen@proton.me>
2026-08-01 02:22:20 +0000

    Refactor main function and add print_report for structured output

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from stats import chars_dict_to_sorted_list, get_chars_dict, get_num_words
+from stats import (
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+    get_num_words,
+)
 
 
 def main() -> None:
@@ -7,13 +11,28 @@ def main() -> None:
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    print(f"Found {num_words} total words")
-    print(chars_sorted_list)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path: str) -> str:
     with open(path) as f:
         return f.read()
+
+
+def print_report(
+    book_path: str, num_words: int, chars_sorted_list: list[tuple[str, int]]
+) -> None:
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char, count in chars_sorted_list:
+        if not char.isalpha():
+            continue
+        print(f"{char}: {count}")
+
+    print("============= END ===============")
 
 
 main()
