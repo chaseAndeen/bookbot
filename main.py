86e5a6068b96d09085ce881e239d0daffa736627
Chase Andeen <chase.andeen@proton.me>
2026-08-01 02:02:44 +0000

    Refactor main and stats functions to improve word and character counting

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from stats import word_count
-from stats import char_count
+from stats import chars_dict_to_sorted_list, get_chars_dict, get_num_words
 
 
-def get_book_text(filepath: str) -> str:
-    with open(filepath) as file:
-        data = file.read()
-    return data
+def main() -> None:
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print(f"Found {num_words} total words")
+    print(chars_sorted_list)
 
-def main():
-    filepath = "books/frankenstein.txt"
-    book_text = get_book_text(filepath)
-    print(f"Found {word_count(book_text)} total words")
-    print(f"{char_count(book_text)}")
+
+def get_book_text(path: str) -> str:
+    with open(path) as f:
+        return f.read()
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,15 +1,26 @@
-def word_count(text: str) -> int:
+def get_num_words(text: str) -> int:
     words = text.split()
     return len(words)
 
-def char_count(text: str) -> dict[str, int]:
-    char_totals = {}
-    chars = list(text.lower())
 
-    for c in chars:
-        if c not in char_totals:
-            char_totals[c] = 1
+def get_chars_dict(text: str) -> dict[str, int]:
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            char_totals[c] += 1
+            chars[lowered] = 1
+    return chars
 
-    return char_totals
+
+def sort_on(char_count: tuple[str, int]) -> int:
+    return char_count[1]
+
+
+def chars_dict_to_sorted_list(num_chars_dict: dict[str, int]) -> list[tuple[str, int]]:
+    chars_list: list[tuple[str, int]] = []
+    for char in num_chars_dict:
+        count = num_chars_dict[char]
+        chars_list.append((char, count))
+    return sorted(chars_list, reverse=True, key=sort_on)
